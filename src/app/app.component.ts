13761f4535c3755566527e5f8ef7c792977d34a8
Will <wfoster12@gmail.com>
2020-01-25 23:02:57 +0000

    Password generation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  length = 0;
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)){
+      this.length = parsedValue;
+    } 
+  }
+
+  onChangeUseLetters(){
+    this.useLetters = !this.useLetters
+  }
+  onChangeUseNumbers(){
+    this.useNumbers = !this.useNumbers
+  }
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols
+  }
 
   onButtonClick(){
-    this.password= 'Test123'
+    let validChars = '';
+    const numbers = '1234567890'
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    const symbols = '!@£$^&*()'
+
+    if(this.useLetters){
+      validChars += letters;
+    }
+    if(this.useNumbers){
+      validChars += numbers;
+    }
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i =0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 }
